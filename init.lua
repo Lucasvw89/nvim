2cb9d6b449ff60aa6ba22c1432b3248fd0d97cee
Lucas <lucasvw89@gmail.com>
2024-02-27 00:24:19 -0300

    added Telescope :)

--- init.lua
+++ init.lua
@@ -12,4 +12,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    require "Telescope"
 })
--- lua/custom/plugins/mappings.lua
+++
@@ -1,4 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-return {
-  keymap("n", "<C-t>", ":Neotree toggle<CR>", { noremap = true, silent = true })
-}
