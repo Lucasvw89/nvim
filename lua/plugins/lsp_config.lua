2594f0af335fee52461bfa0b8044e169259b2140
Lucas <lucasvw89@gmail.com>
2024-03-03 14:24:48 -0300

    small details

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,10 +1,12 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         opts = {},
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
         opts = {
             ensure_installed = {
                 "lua_ls",
@@ -14,6 +16,7 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
