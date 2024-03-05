2b387be1d6329d76286d6bdf6999497054f5faff
Lucas <lucasvw89@gmail.com>
2024-03-05 18:20:30 -0300

    better lsp (using handlers)

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -13,19 +13,31 @@ return {
                 "clangd",
             },
             auto_install = true,
+            handlers = {
+                function (server)
+                    require("lspconfig")[server].setup {
+                        cpabilities = require("cmp_nvim_lsp").default_capabilities()
+                    }
+                end,
+            },
         },
     },
     {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
+
+
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                settings = {
+                       Lua = {
+                           diagnostics = {
+                               globals = { "vim" }
+                           }
+                       }
+                   }
             })
 
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
