92d922c536c293b30f8dd34d9ffb4557ab0fee60
Lucas <lucasvw89@gmail.com>
2024-03-11 00:26:25 -0300

    added more custom mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>qq", ":wqa!<CR>", {})
 
-vim.keymap.set("n", ";", ":", {})
+vim.keymap.set({"n", "v"}, ";", ":", {})
 vim.keymap.set({"n", "v", "i"}, "<C-c>", "<Esc>", {})
 
 vim.keymap.set("n", "<C-h>", "<C-w>h", {})
--- lua/plugins/Telescope.lua
+++ lua/plugins/Telescope.lua
@@ -1,3 +1,15 @@
+vim.api.nvim_create_user_command('ConfigFiles',
+    function ()
+        local curr_os = vim.loop.os_uname().sysname
+        if curr_os == "Windows_NT" then
+            require('telescope.builtin').find_files({ cwd = "~\\AppData\\Local\\nvim" })
+        else
+            require('telescope.builtin').find_files({ cwd = "~/config/nvim" })
+        end
+    end,
+  {}
+)
+
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
@@ -7,5 +19,6 @@ return {
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, {})
+        vim.keymap.set('n', '<leader>fc', ':ConfigFiles<CR>', {})
     end,
 }
