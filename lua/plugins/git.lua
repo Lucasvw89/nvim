bbfcc1295eaa7af2f8eaed8db3904f591c125bd1
Lucas <lucasvw89@gmail.com>
2024-03-18 21:37:07 -0300

    new commit command

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,9 +7,8 @@ return {
         config = function ()
             require("gitsigns").setup()
             vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>gaa", ":Git add .<CR>", {})
             vim.keymap.set("n", "<leader>gps", ":Git push<CR>", {})
-            vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+            vim.keymap.set("n", "<leader>gc", ":Git add .| Git commit<CR>", {})
         end,
     }
 }
