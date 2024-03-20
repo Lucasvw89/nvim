036aac805a8f8926587c45160e53040c06f328b4
Lucas <lucasvw89@gmail.com>
2024-03-20 20:19:27 -0300

    new mapping for windows and terminals

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,12 +1,20 @@
+-- convenience
 vim.keymap.set("n", "<leader>qq", ":wqa!<CR>", {})
 
 vim.keymap.set({"n", "v"}, ";", ":", {})
 vim.keymap.set({"n", "v", "i"}, "<C-c>", "<Esc>", {})
 
+-- windows and terminals
+-- move around
 vim.keymap.set("n", "<C-h>", "<C-w>h", {})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {})
 
-vim.keymap.set("n", "<leader>-", ":split<CR><C-j>", {})
-vim.keymap.set("n", "<leader>_", ":vsplit<CR><C-l>", {})
+-- split windows
+vim.keymap.set("n", "<leader>h", ":split<CR> | <C-w>= | <C-w>j", {})
+vim.keymap.set("n", "<leader>v", ":vsplit<CR> | <C-w>= | <C-w>l", {})
+
+-- create terminals
+vim.keymap.set("n", "<A-h>", ":split<CR> | <C-w>j | :term<CR>", {})
+vim.keymap.set("n", "<A-v>", ":vsplit<CR> | <C-w>l | :term<CR>", {})
